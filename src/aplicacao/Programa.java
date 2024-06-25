c02d4deccede404c6670145ee400f68dd59c36e1
Henrique Fidelis <fidelis.henrique.jh@gmail.com>
2024-06-25 10:29:16 -0300

    Criação do projeto

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,16 @@
+package aplicacao;
+
+/**
+ * Classe principla do projeto
+ * @author Henrique Fidelis
+ * @since Classe criada em 25/06/2024
+ */
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+
+	}// fim do main
+
+}// fim da classe
