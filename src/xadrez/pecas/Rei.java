0c3af587bf6aaf0e342d09ad9f2a4c13221562d7
Henrique Fidelis <fidelis.henrique.jh@gmail.com>
2024-07-05 16:40:11 -0300

    Implementando possíveis movimentos do Rei

--- src/xadrez/pecas/Rei.java
+++ src/xadrez/pecas/Rei.java
@@ -1,5 +1,6 @@
 package xadrez.pecas;
 
+import jogoDeTabuleiro.Posicao;
 import jogoDeTabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PecaDeXadrez;
@@ -23,10 +24,67 @@ public class Rei extends PecaDeXadrez{
 		return "R";
 	}
 
+	// Método para checar se o rei pode se mexer para uma determinada posição
+	private boolean checarMovimentoPossivel(Posicao posicao) {
+		PecaDeXadrez p = (PecaDeXadrez) getTabuleiro().retornarPeca(posicao);
+		return p == null || p.getCor() != getCor();
+	}
+
 	// Método sobrescrito para definir os movimentos do Rei
 	@Override
 	public boolean[][] definirMovimentosPossiveis() {
 		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+		Posicao aux = new Posicao(0, 0);
+
+		// Casas acima da peça
+		aux.inserirValores(posicao.getLinha() - 1, posicao.getColuna());
+		if (getTabuleiro().checarPosicao(aux) && checarMovimentoPossivel(aux)) {
+			mat[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Casas abaixo da peça
+		aux.inserirValores(posicao.getLinha() + 1, posicao.getColuna());
+		if (getTabuleiro().checarPosicao(aux) && checarMovimentoPossivel(aux)) {
+			mat[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Casas à esquerda da peça
+		aux.inserirValores(posicao.getLinha(), posicao.getColuna() - 1);
+		if (getTabuleiro().checarPosicao(aux) && checarMovimentoPossivel(aux)) {
+			mat[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Casas à direita da peça
+		aux.inserirValores(posicao.getLinha(), posicao.getColuna() + 1);
+		if (getTabuleiro().checarPosicao(aux) && checarMovimentoPossivel(aux)) {
+			mat[aux.getLinha()][aux.getColuna()] = true;
+		}
+
+		// Casas à noroeste da peça
+		aux.inserirValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		if (getTabuleiro().checarPosicao(aux) && checarMovimentoPossivel(aux)) {
+			mat[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Casas à nordeste da peça
+		aux.inserirValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		if (getTabuleiro().checarPosicao(aux) && checarMovimentoPossivel(aux)) {
+			mat[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Casas à sudoeste da peça
+		aux.inserirValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		if (getTabuleiro().checarPosicao(aux) && checarMovimentoPossivel(aux)) {
+			mat[aux.getLinha()][aux.getColuna()] = true;
+		}
+		
+		// Casas à sudeste da peça
+		aux.inserirValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		if (getTabuleiro().checarPosicao(aux) && checarMovimentoPossivel(aux)) {
+			mat[aux.getLinha()][aux.getColuna()] = true;
+		}
+
 		return mat;
 	}
 
